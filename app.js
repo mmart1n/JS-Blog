4675e1a67a5a4e171a4a9b82ece3807ff08cd730
mmart1n <m.ewtimow@gmail.com>
2019-09-15 09:16:26 +0300

    add equal registerHelper

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const config = require('./config/config.js');
 const app = express();
 const hbs = require('hbs');
 hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
+hbs.registerHelper('ifeq', function (a, b, options) {
+    if (a == b) { return options.fn(this); }
+    return options.inverse(this);
+});
 
 
 require('./config/express')(app, config);
